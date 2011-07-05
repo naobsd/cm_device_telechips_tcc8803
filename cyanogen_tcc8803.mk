eceda68b4f47765dd0de77cea7ac2c8614148feb
FUKAUMI Naoki <naobsd@gmail.com>
2011-07-05 23:36:22 +0900

    7.1.0-RC1

--- cyanogen_tcc8803.mk
+++ cyanogen_tcc8803.mk
@@ -29,10 +29,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC0-TCC8803
+            ro.modversion=CyanogenMod-7.1.0-RC1-TCC8803
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC0-TCC8803-KANG
+            ro.modversion=CyanogenMod-7.1.0-RC1-TCC8803-KANG
     endif
 endif
 
